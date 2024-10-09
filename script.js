8b37dc0c2630f470dd6997eb6894409b66ba53aa
_vision <shefroy.rowe@gmail.com>
2024-10-08 22:12:05 -0400

    query displays and buttons, create object and populate with arithmetic functions

---
+++ script.js
@@ -0,0 +1,25 @@
+//output
+const primaryDisplay = document.querySelector('.primary-display');
+const secondaryDisplay = document.querySelector('.primary-display');
+//keys
+const allKeys = document.querySelectorAll('.key');
+
+//arithmetic opperations
+const calcFunctions = {
+    sum: (a, b) => {
+        return a + b;
+    },
+    multiply: (a, b) => {
+        return a * b;
+    },
+    subtract: (a, b) => {
+        return a - b;
+    },
+    divide: (a, b) => {
+        return a / b;
+    },
+};
+
+const calculator = (leftHand, opperator, rightHand) => {
+
+}
