5d6d71f791fa7bdc64950b5fc49b1d889a4627f6
_vision <shefroy.rowe@gmail.com>
2024-10-12 18:34:23 -0400

    add footer to html, clean up code, add feature note to js (end of document)

--- script.js
+++ script.js
@@ -1,17 +1,16 @@
+//input
+const allKeys = document.querySelectorAll('.key');
 //output
 const mainDisplay = document.querySelector('.main-display');
 const primaryDisplay = document.querySelector('.primary-display');
 
-//input
-const allKeys = document.querySelectorAll('.key');
 //helper variables
 let leftHand = '';
 let opperator = '';
 let rightHand = '';
-
 let limitFixed = 0;
 
-//arithmetic opperations
+//arithmetic opperations object
 const calcFunctions = {
     sum: (a, b) => {
         return a + b;
@@ -27,29 +26,36 @@ const calcFunctions = {
     },
 };
 
+//access arithmetic opperations object 
 //calculate based on opperator selected
 const operate = (leftHand, opperator, rightHand) => {
+    //store result from any 
     let result = 0;
+
+    //addition
     if (opperator === '+') {
         result = calcFunctions.sum(leftHand, rightHand);
     }
+    //subtraction
     if (opperator === '-') {
         result = calcFunctions.subtract(leftHand, rightHand);
     }
+    //multiplication
     if (opperator === '*') {
         result = calcFunctions.multiply(leftHand, rightHand);
     }
+    //divide
     if (opperator === '/') {
         result = calcFunctions.divide(leftHand, rightHand);
     }
 
+    //return result of equation
     return result;
 };
 
-//query auxilary key values
+//query key values
 allKeys.forEach(key => {
     key.addEventListener("click", (e) => {
-
 
         switch (e.target.id) {
             case '1':
@@ -62,21 +68,17 @@ allKeys.forEach(key => {
             case '8':
             case '9':
             case '0':
-                if (!opperator && !rightHand) {
+                if (!opperator &&
+                    !rightHand) {
                     leftHand += e.target.id;
-                    primaryDisplay.textContent = `${leftHand}`;
+                    primaryDisplay.textContent =
+                        `${leftHand}`;
 
-                    console.log("left hand: ", leftHand);
-                    console.log("opperator: ", opperator);
-                    console.log("right hand: ", rightHand);
-                } else if (leftHand && opperator) {
+                } else if (leftHand &&
+                    opperator) {
                     rightHand += e.target.id;
                     primaryDisplay.textContent =
-                     `${leftHand} ${opperator} ${rightHand}`;
-
-                    console.log("left hand: ", leftHand);
-                    console.log("opperator: ", opperator);
-                    console.log("right hand: ", rightHand);
+                        `${leftHand} ${opperator} ${rightHand}`;
                 }
                 break;
             case '+':
@@ -85,12 +87,8 @@ allKeys.forEach(key => {
             case '*':
                 if (leftHand && !opperator) {
                     opperator = e.target.id;
-                    primaryDisplay.textContent = 
-                    `${leftHand} ${opperator} ${rightHand}`;
-
-                    console.log("left hand: ", leftHand);
-                    console.log("opperator: ", opperator);
-                    console.log("right hand: ", rightHand);
+                    primaryDisplay.textContent =
+                        `${leftHand} ${opperator} ${rightHand}`;
                 }
                 break;
             case '.':
@@ -98,14 +96,15 @@ allKeys.forEach(key => {
                     !rightHand &&
                     !leftHand.includes('.')) {
                     leftHand += '.';
-                    primaryDisplay.textContent = 
-                    `${leftHand} ${opperator} ${rightHand}`;
+                    primaryDisplay.textContent =
+                        `${leftHand} ${opperator} ${rightHand}`;
+
                 } else if (opperator &&
                     leftHand &&
                     !rightHand.includes('.')) {
                     rightHand += '.';
-                    primaryDisplay.textContent = 
-                    `${leftHand} ${opperator} ${rightHand}`;
+                    primaryDisplay.textContent =
+                        `${leftHand} ${opperator} ${rightHand}`;
                 }
                 break;
             case 'clear':
@@ -113,36 +112,45 @@ allKeys.forEach(key => {
                 mainDisplay.textContent = '';
                 leftHand = '';
                 opperator = '';
-                rightHand ='';
+                rightHand = '';
                 break;
             case 'back':
                 if (!opperator &&
                     !rightHand) {
                     leftHand = leftHand.slice(0, -1);
-                    primaryDisplay.textContent = 
-                    `${leftHand} ${opperator} ${rightHand}`;
+                    primaryDisplay.textContent =
+                        `${leftHand} ${opperator} ${rightHand}`;
                 } else if (opperator &&
                     !rightHand) {
-                     opperator = '';
-                    primaryDisplay.textContent = 
-                    `${leftHand} ${opperator} ${rightHand}`;
+                    opperator = '';
+                    primaryDisplay.textContent =
+                        `${leftHand} ${opperator} ${rightHand}`;
                 } else if (opperator &&
                     leftHand) {
-                        rightHand = rightHand.slice(0, -1);
-                    primaryDisplay.textContent = 
-                    `${leftHand} ${opperator} ${rightHand}`;
+                    rightHand = rightHand.slice(0, -1);
+                    primaryDisplay.textContent =
+                        `${leftHand} ${opperator} ${rightHand}`;
                 }
                 break;
             case '=':
-                   limitFixed = `${operate(Number(leftHand), opperator, Number(rightHand))}`;
-                   if (String(limitFixed).includes('.')) {
+                //limit decimal places to 0nly 2 after the dot
+                limitFixed = `${operate(Number(leftHand), opperator, Number(rightHand))}`;
+                if (String(limitFixed).includes('.')) {
                     mainDisplay.textContent = Number(limitFixed).toFixed(2);
-                   } else {
+                } else {
                     mainDisplay.textContent = limitFixed;
-                   }
+                }
 
                 break;
 
         }
     });//end eventLister
 });//end forEach loop
+
+///FEATURES TO IMPLIMENT
+/*
+-- when result is being displayed in main display
+    and an opperator ('+','-','/','*') is pressed
+    result should populate the primary display along with the 
+    pressed opperator.
+*/
